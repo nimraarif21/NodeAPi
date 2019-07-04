efef4f3b398fb8c264f06b50d505054f971a1f9b
Nimra Arif <nimra.arif@tintash.com>
2019-07-04 09:46:57 +0500

    Mongoose for MongoDB

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config(); // Sets up dotenv as soon as our application starts
+
+const express = require('express'); 
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = express.Router();
+
+const environment = process.env.NODE_ENV; // development
+const stage = require('./config')[environment];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+if (environment !== 'production') {
+  app.use(logger('dev'));
+}
+
+
+
+const routes = require('./routes/index.js');
+
+app.use('/api/v1', routes(router));
+
+// app.post('/', (req, res) => {
+//   res.json({
+//     test: "success"
+//   })
+// })
+
+app.listen(`${stage.port}`, () => {
+  console.log(`Server now listening at localhost:${stage.port}`);
+});
+
+module.exports = app;
